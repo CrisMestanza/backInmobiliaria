11f4cdd6e332dc811c9298b47c571eefb64c4774
Hzsxz <166932045+Hzsxz@users.noreply.github.com>
2026-02-27 04:00:23 -0500

    security6

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -1,4 +1,4 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -6,4 +6,28 @@ class Migration(migrations.Migration):
 
     dependencies = []
 
-    operations = []
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            database_operations=[],
+            state_operations=[
+                migrations.CreateModel(
+                    name="Usuario",
+                    fields=[
+                        ("idusuario", models.AutoField(primary_key=True, serialize=False)),
+                        ("password", models.CharField(max_length=128)),
+                        ("last_login", models.DateTimeField(blank=True, null=True)),
+                        ("is_superuser", models.BooleanField(default=False)),
+                        ("correo", models.EmailField(max_length=254, unique=True)),
+                        ("nombre", models.CharField(blank=True, max_length=60, null=True)),
+                        ("estado", models.IntegerField(blank=True, default=1, null=True)),
+                        ("is_active", models.BooleanField(default=True)),
+                        ("is_staff", models.BooleanField(default=False)),
+                    ],
+                    options={
+                        "db_table": "usuario",
+                        "managed": False,
+                    },
+                )
+            ],
+        )
+    ]
